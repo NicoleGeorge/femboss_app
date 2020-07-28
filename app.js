9b25def8eb2e28def6c181204163fb5f9ee8c669
AdorkablyNicole <hello@nicolegeorge.me>
2020-07-28 13:33:25 +0930

    db connected, added .env file

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const express = require('express');
+
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = express();
+// configuring the db
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('DB Connected');
+
+    mongoose.connection.on('err', (err) => {
+      console.log(`DB connection error: ${err.message}`);
+    });
+  });
 
 app.get('/', (req, res) => res.send('app is running'));
 

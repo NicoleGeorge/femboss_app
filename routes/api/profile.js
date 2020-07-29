6dbb2ac6b7b624f1e65d6eddc8938e1be2e1a164
AdorkablyNicole <hello@nicolegeorge.me>
2020-07-29 15:01:32 +0930

    adding user education added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -28,7 +28,7 @@ router.get('/me', auth, async (req, res) => {
   }
 });
 
-// access workflow
+// USER PROFILE workflow
 // @route           POST api/profile==> based on the user id from the token
 // @description     Create or update a user profile
 // @access          Private
@@ -114,7 +114,6 @@ router.post(
   }
 );
 
-// access workflow
 // @route           GET api/profile==> based on the user id from the token
 // @description     Get all user profile
 // @access          Public
@@ -129,7 +128,6 @@ router.get('/', async (req, res) => {
   }
 });
 
-// access workflow
 // @route           GET api/profile/:user_id
 // @description     Get user profile by iserid
 // @access          Public
@@ -153,7 +151,6 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
-// access workflow
 // @route           DELETE api/profile ==> based on the user id from the token
 // @description     Delete a user profile, user and posts
 // @access          Private
@@ -173,7 +170,7 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
-// access workflow
+// USER EXPERINCES  workflow
 // @route           PUT api/profile/experience ==> based on the user id from the token
 // @description     Add profile experiences
 // @access          Private
@@ -227,7 +224,6 @@ router.put(
   }
 );
 
-// access workflow
 // @route           DELETE api/profile/experience/:exp_id ==> based on the user id from the token
 // @description     Delete profile experiences
 // @access          Private
@@ -241,6 +237,84 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
       .indexOf(req.params.exp_id);
 
     profile.experience.splice(deleteIndex, 1);
+
+    await profile.save();
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// USER EDUCATION workflow
+// @route           PUT api/profile/education ==> based on the user id from the token
+// @description     Add profile education
+// @access          Private
+
+router.put(
+  '/education',
+  [
+    auth,
+    [
+      check('school', 'School is required').not().isEmpty(),
+      check('degree', 'Degree is required').not().isEmpty(),
+      check('fieldofstudy', 'Field of Study is required').not().isEmpty(),
+      check('from', 'From date is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newEducation = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.education.unshift(newEducation);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+// @route           DELETE api/profile/education/:edu_id ==> based on the user id from the token
+// @description     Delete profile education
+// @access          Private
+
+router.delete('/education/:edu_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    const deleteIndex = profile.education
+      .map((item) => item.id)
+      .indexOf(req.params.edu_id);
+
+    profile.education.splice(deleteIndex, 1);
 
     await profile.save();
     res.json(profile);

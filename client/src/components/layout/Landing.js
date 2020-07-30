4ab76cd236a1754116ec6619d9331d2e53094800
AdorkablyNicole <hello@nicolegeorge.me>
2020-07-30 17:16:37 +0930

    components folder => auth + layouts

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 export const Landing = () => {
-    return (
-        <section class="landing">
-        <div class="dark-overlay">
-          <div class="landing-inner">
-            <h1 class="x-large">what is femBOSS?</h1>
-            <p class="lead">
-              femBOSS is a social network for ambitious #womenInTech. 
-            </p>
-            <div class="buttons">
-              <a href="register.html" class="btn btn-primary">Join</a>
-              <a href="login.html" class="btn btn-light">Log in</a>
-            </div>
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>what is femBOSS?</h1>
+          <p className='lead'>
+            femBOSS is a social network for ambitious #womenInTech.
+          </p>
+          <div className='buttons'>
+            <a href='register.html' className='btn btn-primary'>
+              Join
+            </a>
+            <a href='login.html' className='btn btn-light'>
+              Log in
+            </a>
           </div>
         </div>
-      </section>
-    )
-}
+      </div>
+    </section>
+  );
+};
 
-export default Landing
+export default Landing;

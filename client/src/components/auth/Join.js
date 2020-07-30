1a8f361b4cafb8b2da3cf3652bc00ad68009871b
AdorkablyNicole <hello@nicolegeorge.me>
2020-07-30 18:24:49 +0930

    user ccan be added to db from frontend signup form

--- client/src/components/auth/Join.js
+++ client/src/components/auth/Join.js
@@ -1,11 +1,112 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react';
+import axios from 'axios';
 
 export const Join = () => {
-    return (
-        <div>
-            <h1>join</h1>
-        </div>
-    )
-}
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  });
 
-export default Join
+  // destructure the state
+
+  const { name, email, password, passwordConfirm } = formData;
+
+  //   update the name, use the spread operator to copy formData, change name to the value of the input
+  const onChange = (e) =>
+    //   [e.target.name] => using this as a key, so I can use the onChange for every inpout field
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  // function so the submit form doesn't refresh
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    // validating paswords => making sure they both match
+
+    if (password !== passwordConfirm) {
+      console.log('passwords do not match');
+    } else {
+      const newUser = {
+        name,
+        email,
+        password,
+      };
+      try {
+        const config = {
+          headers: {
+            'Content-type': 'application/json'
+          }
+        }
+        const body = JSON.stringify(newUser);
+        const res = await axios.post('/api/users', body, config); 
+        console.log(res.data);
+      } catch (error) {
+        console.error(error.response.data);
+      }
+    }
+  };
+
+  return (
+    <Fragment>
+      <section className='join-hero'>
+        <h1 className='large text-primary '>Join</h1>
+        <p className='lead'>
+          <i className='fas fa-magic'></i> Create Your Account
+        </p>
+        <form className='form' onSubmit={(e) => onSubmit(e)}>
+          <div className='form-group'>
+            <input
+              type='text'
+              placeholder='Name'
+              name='name'
+              vale={name}
+              onChange={(e) => onChange(e)}
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='email'
+              placeholder='Email Address'
+              name='email'
+              vale={email}
+              onChange={(e) => onChange(e)}
+              required
+            />
+            <small className='form-text'>
+              This site uses Gravatar so if you want a profile image, use a
+              Gravatar email
+            </small>
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              placeholder='Password'
+              name='password'
+              vale={password}
+              onChange={(e) => onChange(e)}
+              minLength='6'
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              placeholder='Confirm Password'
+              name='passwordConfirm'
+              value={passwordConfirm}
+              onChange={(e) => onChange(e)}
+              minLength='6'
+            />
+          </div>
+          <input type='submit' className='btn btn-primary' value='Register' />
+        </form>
+        <p className='my-1'>
+          Already have an account? <a href='login.html'>Sign In</a>
+        </p>
+      </section>
+    </Fragment>
+  );
+};
+
+export default Join;

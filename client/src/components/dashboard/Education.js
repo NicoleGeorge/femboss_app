ee49a120a13a5e265e4451a6b107815a78ba40f1
AdorkablyNicole <hello@nicolegeorge.me>
2020-08-02 00:28:49 +0930

    updated

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -2,29 +2,31 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Moment from 'react-moment';
+import { deleteEducation } from '../../actions/profile';
 
-const Education = ({ education }) => {
+const Education = ({ education, deleteEducation }) => {
   const educations = education.map((edu) => (
     <tr key={edu._id}>
       <td>{edu.school}</td>
       <td className='hide-sm'>{edu.degree}</td>
       <td>
         <Moment format='DD/MM/YY'>{edu.from}</Moment> -{' '}
-        {edu.to === null ? (
-          ' Now'
-        ) : (
-          <Moment format='DD/MM/YY'>{edu.to}</Moment>
-        )}
+        {edu.to === null ? ' Now' : <Moment format='DD/MM/YY'>{edu.to}</Moment>}
       </td>
       <td>
-        <button className='btn btn-danger'>Delete</button>
+        <button
+          onClick={() => deleteEducation(edu._id)}
+          className='btn btn-danger'
+        >
+          Delete
+        </button>
       </td>
     </tr>
   ));
 
   return (
     <Fragment>
-      <h2 className='my-2'>My Education Journey</h2>
+      <h2 className='my-2'>My Education</h2>
       <table className='table'>
         <thead>
           <tr>
@@ -42,6 +44,7 @@ const Education = ({ education }) => {
 
 Education.propTypes = {
   education: PropTypes.array.isRequired,
+  deleteEducation: PropTypes.func.isRequired,
 };
 
-export default Education;
+export default connect(null, { deleteEducation })(Education);
--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -26,7 +26,7 @@ const Experience = ({ experience, deleteExperience }) => {
 
   return (
     <Fragment>
-      <h2 className='my-2'>My Experience Journey</h2>
+      <h2 className='my-2'>My Experience</h2>
       <table className='table'>
         <thead>
           <tr>

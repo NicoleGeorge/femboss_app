13fdc9c2be46cabff05cf0809174a9af185c318d
AdorkablyNicole <hello@nicolegeorge.me>
2020-08-03 18:42:19 +0930

    forum stage 2 ==>  page routes and items rendering

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,12 +3,27 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/post';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem'
 
-const Posts = ({ getPosts, post: { posts } }) => {
+const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return <div></div>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Forum</h1>
+      <p className='lead'>
+        <i className='fas fa-users'></i> Welcome to the femBOSS Network!
+      </p>
+      <div className='posts'>
+          {posts.map(post => (
+              <PostItem key={post._id} post={post} />
+          ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {

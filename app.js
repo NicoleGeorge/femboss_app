ace2e517196aaff439ce158a1d6d45f6adf1f0ed
AdorkablyNicole <hello@nicolegeorge.me>
2020-08-05 12:04:57 +0930

    getting files ready for deployment

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
+const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -41,7 +42,17 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-app.get('/', (req, res) => res.send('app is running'));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+  // serve the above static index.html file
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+// app.get('/', (req, res) => res.send('app is running')); ===> removed for deployment
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {

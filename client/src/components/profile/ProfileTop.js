9a992d5ea4eb5c2eaf0e7ae8a73371f4f03621d5
AdorkablyNicole <hello@nicolegeorge.me>
2020-08-05 10:54:53 +0930

    updated styling

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -16,8 +16,8 @@ const ProfileTop = ({
     <div className='profile-top p-2'>
       <img className='round-img my-1' src={avatar} alt='' />
       <h1 className='large'>{name}</h1>
-      <p className='lead'>
-        {status} {company && <span> at {company}</span>}
+      <p className='lead-profile-top'>
+        {status}, {company && <span> at {company}</span>}
       </p>
       <p>{location && <span>{location}</span>}</p>
       <div className='icons my-1'>

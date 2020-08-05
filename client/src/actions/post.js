01ceac4881700c0b605d67f92f8859d14e6ba567
AdorkablyNicole <hello@nicolegeorge.me>
2020-08-05 12:30:26 +0930

    updating-addressing error warning in terminal

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -70,7 +70,7 @@ export const removeLike = (id) => async (dispatch) => {
 
 export const deletePost = (id) => async (dispatch) => {
   try {
-    const res = await axios.delete(`/api/posts/${id}`);
+    await axios.delete(`/api/posts/${id}`);
 
     dispatch({
       type: DELETE_POST,
@@ -159,7 +159,7 @@ export const addComment = (postId, formData) => async (dispatch) => {
 
 export const deleteComment = (postId, commentId) => async (dispatch) => {
   try {
-    const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
 
     dispatch({
       type: DELETE_COMMENT,
